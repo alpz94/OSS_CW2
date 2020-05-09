af0afee04b3d2db4611a70930831f58814ee60ce
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-09 23:19:13 +0100

    added values to ID and password (login.php)

--- templates/login.php
+++ templates/login.php
@@ -3,10 +3,10 @@
 
 <form name="frmLogin" action="authenticate.php" method="post">
    Student ID:
-   <input name="txtid" type="text" />
+   <input name="txtid" type="text" value="Enter your ID" />
    <br/>
    Password:
-   <input name="txtpwd" type="password" />
+   <input name="txtpwd" type="password" value="Enter your password"/>
    <br/>
    <input type="submit" value="Login" name="btnlogin" />
 </form>
