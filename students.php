01f68bc43e6b341654f351b3843563a0bf52348f
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-09 21:50:49 +0100

    All records displayed(students.php)+link added to nav

--- students.php
+++ students.php
@@ -2,4 +2,42 @@
 CO551 Open Source Systems -->
 <?php
 
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+
+
+   // check logged in
+   if (isset($_SESSION['id'])) {
+
+      echo template("templates/partials/header.php");
+      echo template("templates/partials/nav.php");
+
+      // Build SQL statment that selects a student's modules
+      $sql = "select * from student;";
+
+      $result = mysqli_query($conn,$sql);
+
+      // prepare page content
+      $data['content'] .= "<table border='1'>";
+      $data['content'] .= "<tr><th colspan='10' align='center'>Student</th></tr>";
+      $data['content'] .= "<tr><th>Student ID</th><th>Password</th><th>DOB</th><th>First Name</th>
+      <th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>";
+      // Display the modules within the html table
+      while($row = mysqli_fetch_array($result)) {
+         $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[password] </td>";
+         $data['content'] .= "<td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td>";
+         $data['content'] .= "<td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td><td> $row[postcode] </td></tr>";
+      }
+      $data['content'] .= "</table>";
+
+      // render the template
+      echo template("templates/default.php", $data);
+
+   } else {
+      header("Location: index.php");
+   }
+
+   echo template("templates/partials/footer.php");
+
 ?>
--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,6 +1,7 @@
 
 <div id="main-nav">
 
+   <a href="students.php">Students</a> |
    <a href="modules.php">My Modules</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
