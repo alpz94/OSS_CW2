9e0f9a4b0e1842dea5fc616027b27f1ee86541b2
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-11 00:31:29 +0100

    minor change

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light blue-gradient fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light blue-gradient">
   <a class="navbar-brand" href="students.php">
   <img src="/OSS_CW2/img/my_logo.png" width="50" height="50" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
