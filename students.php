a133962b26865eec496856e0c14d5b95d72963ea
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-10 23:36:04 +0100

    personalised navbar; completed half of task 2

--- students.php
+++ students.php
@@ -18,18 +18,21 @@ CO551 Open Source Systems -->
 
       $result = mysqli_query($conn,$sql);
 
+      $data['content'] .= "<form method='post' action=''>";
       // prepare page content
       $data['content'] .= "<table border='1'>";
       $data['content'] .= "<tr><th colspan='10' align='center'>Student</th></tr>";
       $data['content'] .= "<tr><th>Student ID</th><th>Password</th><th>DOB</th><th>First Name</th>
-      <th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>";
+      <th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
       // Display the modules within the html table
       while($row = mysqli_fetch_array($result)) {
          $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[password] </td>";
          $data['content'] .= "<td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td>";
-         $data['content'] .= "<td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td><td> $row[postcode] </td></tr>";
+         $data['content'] .= "<td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td><td> $row[postcode] </td><td><input type='checkbox' name='Select[$row[studentid]]' </td></tr>";
       }
       $data['content'] .= "</table>";
+      $data['content'] .= '<input type="submit" name="delete" value="Delete Records">';
+      $data['content'] .= "</form>";
 
       // render the template
       echo template("templates/default.php", $data);
--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,10 +1,26 @@
-
-<div id="main-nav">
-
-   <a href="students.php">Student Records</a> |
-   <a href="modules.php">My Modules</a> |
-   <a href="assignmodule.php">Assign Module</a> |
-   <a href="details.php">My Details</a> |
-   <a href="logout.php">Logout</a>
-
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="students.php">
+  <img src="/OSS_CW2/img/logo.png" width="50" height="50" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="students.php">Student Records</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="modules.php">My Modules</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="assignmodule.php">Assign Module</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="details.php">My Details</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
