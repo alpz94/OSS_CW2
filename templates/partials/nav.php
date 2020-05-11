cb40d4ce8891d0d13332268ca86a59953ea6a525
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-11 01:08:54 +0100

    navbar text-white with new logo matching

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,25 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-light blue-gradient">
   <a class="navbar-brand" href="index.php">
-  <img src="/OSS_CW2/img/my_logo.png" width="50" height="50" alt=""></a>
+  <img src="/OSS_CW2/img/mylogo.png" width="50" height="50" alt=""></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="students.php">Student Records</a>
+        <a class="nav-link text-white" href="students.php">Student Records</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="modules.php">My Modules</a>
+        <a class="nav-link text-white" href="modules.php">My Modules</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="assignmodule.php">Assign Module</a>
+        <a class="nav-link text-white" href="assignmodule.php">Assign Module</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="details.php">My Details</a>
+        <a class="nav-link text-white" href="details.php">My Details</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="logout.php">Logout</a>
+        <a class="nav-link text-white" href="logout.php">Logout</a>
       </li>
     </ul>
   </div>
