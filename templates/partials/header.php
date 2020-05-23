a765131b3cdb139eeefd0f7188263379a2e5afa5
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-23 19:13:30 +0100

    My CSS

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -7,6 +7,8 @@
 
     <title>BNU Student Web Application</title>
     
+    <!-- Ana's CSS -->
+    <link rel="stylesheet" href="css/mycss.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- Font Awesome -->
