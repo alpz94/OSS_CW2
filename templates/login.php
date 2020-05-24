97ef206bb69af409622c070b44c67f52b5e422b2
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-05-24 18:19:00 +0100

    added comment w/ my name (author)

--- templates/login.php
+++ templates/login.php
@@ -1,3 +1,5 @@
+<!-- Author: Ana Lucia Petinga Zorro 
+CO551 Open Source Systems -->
 <section class="container-md">
    <div class="jumbotron text-center mdb-color lighten-2 white-text mx-2 mb-5">
       <h1 class="card-title h1">Login to OSS-CW2</h1>
